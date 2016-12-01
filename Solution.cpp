c72fb2d6bec8fee6845bc9c7264c460d7fcbb583
fafa-npu <zhaohua00@live.com>
2016-12-01 18:52:19 +0800

    完成 48. Rotate Image
    Signed-off-by: fafa-npu <zhaohua00@live.com>

--- Solution.cpp
+++ Solution.cpp
@@ -458,3 +458,17 @@ string Solution::countAndSay(int n) {
     }
     return seq;
 }
+
+void Solution::rotate(vector<vector<int>> &matrix) {
+    int tmp;
+    int n = matrix.size();
+    for (size_t i = 0; i < n / 2; i++) {
+        for (size_t j = i; j < n - i - 1; j ++) {
+            tmp = matrix[i][j];
+            matrix[i][j] = matrix[n - j - 1][i];
+            matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+            matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+            matrix[j][n - i - 1] = tmp;
+        }
+    }
+}
--- Solution.h
+++ Solution.h
@@ -182,6 +182,16 @@ public :
      * @return
      */
     string countAndSay(int n);
+
+    /**
+     * You are given an n x n 2D matrix representing an image.
+     * Rotate the image by 90 degrees (clockwise).
+     * Follow up:
+     * Could you do this in-place?
+     * @param matrix
+     */
+    void rotate(vector<vector<int>> & matrix);
+
 };
 
 
