4ad404a612b6230a6c20a0bbde1f622c38073341
fafa-npu <zhaohua00@live.com>
2016-12-20 16:40:37 +0800

    完成 62. Unique Path
    Signed-off-by: fafa-npu <zhaohua00@live.com>

--- Solution.cpp
+++ Solution.cpp
@@ -1272,3 +1272,19 @@ vector<vector<int>> Solution::generateMatrix(int n) {
     }
     return result;
 }
+
+int Solution::uniquePaths(int m, int n) {
+    vector<vector<int>> paths(m, vector<int>(n));
+    for (int i = 0; i < m; i++) {
+        paths[i][0] = 1;
+    }
+    for (int j = 0; j < n; j++) {
+        paths[0][j] = 1;
+    }
+    for (int i = 1; i < m; i++) {
+        for (int j = 1; j < n; j++) {
+            paths[i][j] = paths[i - 1][j] + paths[i][j-1];
+        }
+    }
+    return paths[m-1][n-1];
+}
