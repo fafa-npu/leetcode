d0f83406624896c88d10bdebe7b92529d752f9a8
zhaohua <zhaohua00@live.com>
2016-12-23 22:24:01 +0800

    修改 72. Edit Distance

--- Solution.cpp
+++ Solution.cpp
@@ -1587,7 +1587,7 @@ int Solution::minDistance(string word1, string word2) {
         dp[0][i] = i;
     }
     for (int i = 1; i <= length1; i++) {
-        for (int j = 2; j <= length2; j++) {
+        for (int j = 1; j <= length2; j++) {
             if (word1[i - 1] == word2[j - 1]) {
                 dp[i][j] = dp[i - 1][j - 1];
             } else {
