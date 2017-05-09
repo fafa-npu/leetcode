f1df4d0e18c520da1b407b2b643800caaa297dff
fafa-npu <zhaohua00@live.com>
2017-05-09 20:29:28 +0800

    完成 ： 396. Rotate Function

--- Solution.cpp
+++ Solution.cpp
@@ -3776,3 +3776,21 @@ int Solution::findPaths(int m, int n, int N, int i, int j) {
     }
     return curpaths[i+1][j+1].second;
 }
+
+int Solution::maxRotateFunction(vector<int> & A) {
+	if (A.empty()) {
+		return 0;
+	}
+	int n = A.size();
+	int f = 0;
+	int sum = accumulate(A.begin(), A.end(), 0);
+	for (int i = 0; i < n; i++) {
+		f += i * A[i];
+	}
+	int maxSum = INT_MIN;
+	for (int i = n - 1; i >= 0; i--) {
+		f = f + sum - n * A[i];
+		maxSum = max(maxSum, f);
+	}
+	return maxSum;
+}
